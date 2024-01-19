f401a96659dd927f09b87f8ed8450eea3c76ef02
Doprez <73259914+Doprez@users.noreply.github.com>
2024-01-19 09:35:47 -0700

    initial commit

---
+++ ThirdPersonPlatformer.WinForms/Form1.cs
@@ -0,0 +1,40 @@
+using Stride.Engine;
+using Stride.Games;
+
+namespace ThirdPersonPlatformer.WinForms;
+
+public partial class Form1 : Form
+{
+	public Form1()
+	{
+		InitializeComponent();
+
+		// Create SDL window using panel
+		_sdlWindow = new Stride.Graphics.SDL.Window("Embedded Stride Window", panel1.Handle);
+		var context = new GameContextSDL(_sdlWindow, _sdlWindow.Size.Width, _sdlWindow.Size.Height);
+
+		// Start the game
+		_game = new();
+
+		_game.WindowMinimumUpdateRate.MinimumElapsedTime = new TimeSpan(5);
+		_game.MinimizedMinimumUpdateRate.MinimumElapsedTime = new TimeSpan(5);
+
+		Task.Factory.StartNew(() =>
+		{
+			// Must move this off current thread or the form will hang.
+			_game.Run(context);
+		}, TaskCreationOptions.LongRunning);
+	}
+
+	private void Panel1_Layout(object? sender, LayoutEventArgs e)
+	{
+		if (sender is Control control)
+		{
+			_sdlWindow.Size = new(control.Width, control.Height);
+		}
+	}
+
+
+	private Game _game;
+	private Stride.Graphics.SDL.Window _sdlWindow;
+}
